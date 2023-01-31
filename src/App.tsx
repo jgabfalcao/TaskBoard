4ae577b71bb179f6c59e79fe05416f6ea3d4c2e8
João Falcão <jgabfalcao@gmail.com>
2023-01-31 20:46:30 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./components/button";
+import Form from "./components/form";
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
