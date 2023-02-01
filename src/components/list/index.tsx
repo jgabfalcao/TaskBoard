c6fa51901d2814f882bd938330cd108e5918c707
João Falcão <jgabfalcao@gmail.com>
2023-01-31 21:02:16 -0300

    Creating the list component

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -3,7 +3,25 @@ import React from 'react';
 function List(){
   return(
     <aside>
-        
+        <h2>Estudos do dia</h2>
+        <ul>
+          <li>
+            <h3>
+              React
+            </h3>
+            <span>
+              02:00:00
+            </span>
+          </li>
+          <li>
+            <h3>
+              TypeScript
+            </h3>
+            <span>
+              01:00:00
+            </span>
+          </li>
+        </ul>
     </aside>
   )
 }
