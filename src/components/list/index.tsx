bd4ed96fb32b03da718d33e0e87b2c72e6d94bc9
João Falcão <jgabfalcao@gmail.com>
2023-02-01 21:55:26 -0300

    Insert off map to go through all the array and make reactive html

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
 
 function List(){
+  const tasks = [{
+    tarefa: 'React',
+    tempo: '02:00:00'
+  }, {
+    tarefa: 'TypeScript',
+    tempo: '01:00:00'
+  }, {
+    tarefa: 'JavaScript',
+    tempo: '03:00:00  '
+  }
+  ]
+
   return(
     <aside>
         <h2>Estudos do dia</h2>
         <ul>
-          <li>
-            <h3>
-              React
-            </h3>
-            <span>
-              02:00:00
-            </span>
-          </li>
-          <li>
-            <h3>
-              TypeScript
-            </h3>
-            <span>
-              01:00:00
-            </span>
-          </li>
+          {tasks.map((item, index) => (
+            <li key={index}>
+              <h3>
+               {item.tarefa}
+              </h3>
+                <span>
+                {item.tempo }
+                </span>
+              
+            </li>
+          ))}
         </ul>
     </aside>
   )
