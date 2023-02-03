8cb088469cd48f254c9e374257f7adc0bbe03b72
João Falcão <jgabfalcao@gmail.com>
2023-02-02 22:20:33 -0300

    Insert of class component whit state

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,10 +1,22 @@
-import React  from "react";
+import { time } from "console";
+import React, { useState }  from "react";
 import Button from "../button";
 import style from './Form.module.scss'
 
-function Form (){
+class Form extends  React.Component{
+  state = {
+    task: "",
+    time: "00:00"
+  }
+
+  insertTask (event: React.FormEvent<HTMLFormElement>){
+    event.preventDefault(); 
+    console.log('state: ', this.state);
+  }
+
+  render(){
     return(
-      <form className={style.newTask}>
+      <form className={style.newTask} onSubmit={this.insertTask.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="task">
             Adicione um novo estudo 
@@ -13,6 +25,8 @@ function Form (){
           type="text"
           name="task"
           id="task"
+          value={this.state.task}
+          onChange={event => this.setState({...this.state, task: event.target.value})}
           placeholder="O que voce quer estudar" required
           />
         </div>
@@ -24,17 +38,22 @@ function Form (){
           type="time"
           step="1"
           name="time"
+          value={this.state.time}
+          onChange={event => this.setState({...this.state, time: event.target.value})}
           id="time"
           min="00:00:00"
           max="01:30:00"
           required
           />
         </div>
-        <Button btnText="Adicionar"/> 
+        <Button 
+        btnText="Adicionar"/> 
         
       </form>
     )
   }
+
+}
 
 
 export default Form;
