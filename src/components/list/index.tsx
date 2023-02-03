51b59abdc9fa8a916617be9acb1839c39fd78c39
João Falcão <jgabfalcao@gmail.com>
2023-02-02 21:39:31 -0300

    Insert of UseState to create reactive components

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,24 +1,27 @@
 import { time } from 'console';
-import React from 'react';
+import React, { useState } from 'react';
 import Item from './item';
 import style from './List.module.scss'
 
 function List(){
-  const tasks = [{
-    task: 'React',
-    time: '02:00:00'
-  }, {
-    task: 'TypeScript',
-    time: '01:00:00'
-  }, {
-    task: 'JavaScript',
-    time: '03:00:00  '
-  }
-  ]
+
+  const [tasks, setTasks] = useState([{
+  task: 'React',
+  time: '02:00:00'
+}, {
+  task: 'TypeScript',
+  time: '01:00:00'
+}, {
+  task: 'JavaScript',
+  time: '03:00:00  '
+}]);
 
   return(
     <aside className={style.listTasks}>
-        <h2>Estudos do dia</h2>
+        <h2 onClick={() =>{
+          setTasks([...tasks, {task: "Estudar estado", time: "05:00:00"}])
+        }}>
+          Estudos do dia</h2>
         <ul>
           {tasks.map((item, index) => (
            <Item 
