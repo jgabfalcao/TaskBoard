13c393866108d564d3627fdf167a46b0473ecf72
João Falcão <jgabfalcao@gmail.com>
2023-02-06 21:54:27 -0300

    Insert of new states of the interface ITask[

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,33 +1,18 @@
 import { time } from 'console';
-import React, { useState } from 'react';
 import Item from './item';
 import style from './List.module.scss'
+import { ITask } from '../../types/task';
 
-function List(){
 
-  const [tasks, setTasks] = useState([{
-  task: 'React',
-  time: '02:00:00'
-}, {
-  task: 'TypeScript',
-  time: '01:00:00'
-}, {
-  task: 'JavaScript',
-  time: '03:00:00  '
-}]);
-
+function List({ tasks }: {tasks: ITask[]}){
   return(
     <aside className={style.listTasks}>
-        <h2 onClick={() =>{
-          setTasks([...tasks, {task: "Estudar estado", time: "05:00:00"}])
-        }}>
-          Estudos do dia</h2>
+        <h2> Estudos do dia </h2>
         <ul>
           {tasks.map((item, index) => (
            <Item 
            key={index}
-           task={item.task}
-           time={item.time}
+           {...item}
            />
           ))}
         </ul>
